d2fa7678b08fced448825a22cb329302bde14daa
Mariano Lopez <m_villa@hotmail.com>
2019-09-22 19:28:50 -0300

    Zuul Proxy + Eureka Server: enable auto routing & setup fetch interval

--- gateway/src/main/kotlin/com/z/gateway/GatewayApplication.kt
+++ gateway/src/main/kotlin/com/z/gateway/GatewayApplication.kt
@@ -3,9 +3,11 @@ package com.z.gateway
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableZuulProxy
 class GatewayApplication
 
 fun main(args: Array<String>) {
